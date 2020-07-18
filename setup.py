765d2eaecaac7417c2b03128bbd596c4f4a91895
Yamada <>
2020-07-18 19:22:41 +0900

    Add classifiers

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(name="b4tf",
       version="0.0.0",
       description="Bayes for TensorFlow",
       install_requires=["tensorflow>=2","tensorflow_probability"],
-      packages=find_packages())
+      packages=find_packages(),
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Intended Audience :: Science/Research",
+                     "License :: OSI Approved :: MIT License",
+                     "Programming Language :: Python :: 3 :: Only",
+                     "Topic :: Scientific/Engineering :: Artificial Intelligence"])
