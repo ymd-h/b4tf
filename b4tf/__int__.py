6b248bd7335d9e941277f20489503111030a8cfe
Yamada <>
2020-07-18 15:19:36 +0900

    Add create_units

--- b4tf/__int__.py
+++ b4tf/__int__.py
@@ -2,6 +2,32 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 
+def create_model(units,cls,input_shape=(1,),activation=tf.nn.leaky_relu,**kwargs):
+    """
+    Create model (network) with specified size units
+
+    Parameters
+    ----------
+    units : array-like of int
+        Hidden and output units
+    cls : tf.keras.Layer
+        Class for each layer
+    input_shape : array-like of int
+        Input shape. The default value is `(1,)`
+    activation : callable or str
+        Activation function can be used at `tf.keras.Layer`
+
+    Returns
+    -------
+    model : tf.keras.Model
+        Model with specified units
+    """
+    S = tf.keras.Sequential
+    return S([cls(units[0],activation=activation,input_shape=input_shape,**kwargs)] +
+             [cls(i,activation=activation,**kwargs) for i in units[1:-1]] +
+             [cls(units[-1],**kwargs)])
+
+
 def multivariate_normal_fn(loc,relative=0.01):
     """
     Create a function for multivariate normal N(loc, abs(loc)*relative)
