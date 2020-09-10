cd4ff747b365fac48ad826d05c2a9af2cf683ebe
Yamada <ymd_h@outlook.jp>
2020-09-10 14:17:10 +0900

    Start Probabilistic Backpropagation

---
+++ b4tf/models.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+class PBP:
+    """
+    Probabilistic Backpropagation
+
+    References
+    ----------
+    J. M. Hernández-Lobato and R. P. Adams,
+    "Probabilistic Backpropagation for Scalable Learning of Bayesian Neural Networks",
+    arXiv 1502.05336, 2015
+    """
+    def __init__(self,units):
+        pass
+
+    def prob(self,x):
+        pass
