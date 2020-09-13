102fd0abacc7d96c849bd633e54e11477f725f38
Yamada <ymd_h@outlook.jp>
2020-09-13 17:14:43 +0900

    Add unit test for logZ

---
+++ test/test_pbp.py
@@ -0,0 +1,20 @@
+import unittest
+
+from b4tf.models import PBP, PBPLayer, PBPReLULayer
+
+
+class TestPBP(unittest.TestCase):
+    def test_logZ(self):
+        pbp = PBP([2,1])
+
+        diff2 = tf.constant(0.0)
+        v = tf.constant(1.0)
+        pi = tf.math.atan(1.0) * 4
+
+        self.assertEqual(pbp._logZ(diff2,v),
+                         -0.5*tf.math.log(2.0*pi*v))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
