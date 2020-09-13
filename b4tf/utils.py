394791324ce43defff5ca2c262bbe5e14129504d
Yamada <ymd_h@outlook.jp>
2020-09-13 17:34:38 +0900

    b4tf.utils

---
+++ b4tf/utils.py
@@ -0,0 +1,39 @@
+from typing import Iterable
+
+import tensorflow_probability as tfp
+
+class ReciprocalGammaInitializer:
+    """
+    Weight initializers by 1/Gamma(alpha,beta)
+    """
+    def __init__(self,alpha,beta):
+        """
+        Initialize ReciprocalGammaInitializer
+
+        Parameters
+        ----------
+        alpha : int, float or tf.Tensor
+            Parameter for Gamma(alpha,beta)
+        beta : int, float or tf.Tensor
+            Parameter for Gamma(alpha,beta)
+        """
+        self.Gamma = tfp.distributions.Gamma(concentration=alpha,rate=beta)
+
+    def __call__(self,shape: Iterable,dtype=None):
+        """
+        Returns a tensor object initialized as specified by the initializer.
+
+        Parameters
+        ----------
+        shape : Iterable
+            Shape for returned tensor
+        dtype : dtype
+            dtype for returned tensor
+
+
+        Returns
+        -------
+        : tf.Tensor
+            Initialized tensor
+        """
+        return 1.0/self.Gamma(shape,dtype)
