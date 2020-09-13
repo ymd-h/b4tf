6497a8ef047ff8c2a28fb00236730b4ae5522826
Yamada <ymd_h@outlook.jp>
2020-09-13 20:59:36 +0900

    Fix: code algin

--- test/test_pbp.py
+++ test/test_pbp.py
@@ -21,7 +21,8 @@ class TestPBP(unittest.TestCase):
         v = tf.constant(1.0)
         pi = tf.math.atan(1.0) * 4
 
-        self.assertTrue(tf.reduce_all(pbp._logZ(diff2,v) == -0.5*tf.math.log(2.0*pi*v)))
+        self.assertTrue(tf.reduce_all(pbp._logZ(diff2,v) ==
+                                      -0.5*tf.math.log(2.0*pi*v)))
 
 
     def test_fit(self):
