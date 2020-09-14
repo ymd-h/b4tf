70e4ee05ce337b1f2085139672165f70382fca29
Yamada <ymd_h@outlook.jp>
2020-09-14 12:35:37 +0900

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="b4tf",
       author="Hiroyuki Yamada",
-      version="0.0.0",
+      version="0.0.1",
       description="Bayes for TensorFlow",
       install_requires=["tensorflow>=2","tensorflow_probability"],
       packages=find_packages(),
