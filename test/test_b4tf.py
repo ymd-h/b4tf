dcee847b2b0aefc6bca25d8b877f70feea16a64e
Yamada <ymd_h@outlook.jp>
2020-09-14 20:40:21 +0900

    Unit Test for b4tf import

---
+++ test/test_b4tf.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestB4TF(unittest.TestCase):
+    def test_import_b4tf(self):
+        import b4tf
+        pbp = b4tf.PBP([2,1])
+
+    def test_import_PBP(self):
+        import b4tf.PBP as _PBP
+        pbp = _PBP([2,1])
+
+    def test_relative_import(self):
+        from b4tf import PBP as _PBP_
+        pbp = _PBP_([2,1])
+
+
+if __name__ == "__main__":
+    unittest.main()
