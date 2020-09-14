06fbaed4fef02f5153c0326461e0799d53c6a43c
Yamada <ymd_h@outlook.jp>
2020-09-15 08:08:30 +0900

    Check predict and fit

--- test/test_pbp.py
+++ test/test_pbp.py
@@ -47,10 +47,16 @@ class TestPBP(unittest.TestCase):
         y1 = pbp(x1)
         self.assertEqual(y1.shape,(1,1))
 
+        m1,v1 = pbp.predict(x1)
+        pbp.fit(x1,y1)
+
         x2 = tf.constant([[1.0,2.0,3.0],
                           [2.0,3.0,4.0]])
         y2 = pbp(x2)
         self.assertEqual(y2.shape,(2,1))
+
+        m2, v2 = pbp.predict(x2)
+        pbp.fit(x2,y2)
 
     def test_call_with_different_dtype(self):
         pbp = PBP([2,2,1],input_shape=(2,))
@@ -65,6 +71,10 @@ class TestPBP(unittest.TestCase):
         x1 = np.asarray(1.0)
         y1 = pbp(x1)
         self.assertEqual(y1.dtype,tf.float64)
+
+        m,v = pbp.predict(x1)
+        pbp.fit(x1,y1)
+
 
 class TestPBPLayer(unittest.TestCase):
     _class = PBPLayer
