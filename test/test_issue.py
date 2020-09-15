6cb10abdd79ed8ab0368fac8ad6e559573d6ec2f
Yamada <ymd_h@outlook.jp>
2020-09-15 19:34:07 +0900

    Unit Test for input shape

--- test/test_issue.py
+++ test/test_issue.py
@@ -12,6 +12,32 @@ class TestIssue3(unittest.TestCase):
 
         pbp.predict(x)
 
+    def test_shape1_call0(self):
+        pbp = b4tf.models.PBP([2,2,1])
+        x = tf.constant(0.0)
+        pbp(x)
+
+    def test_shape1_call1(self):
+        pbp = b4tf.models.PBP([2,2,1])
+        x = tf.constant((0.0,))
+        pbp(x)
+
+    def test_shape1_call2(self):
+        pbp = b4tf.models.PBP([2,2,1])
+        x = tf.constant([0.0,0.0])
+        pbp(x)
+
+    def test_shape2_call1(self):
+        pbp = b4tf.models.PBP([2,2,1],input_shape=(2,))
+        x = tf.constant([0.0,0.0])
+        pbp(x)
+
+
+    def test_shape2_call2(self):
+        pbp = b4tf.models.PBP([2,2,1],input_shape=(2,))
+        x = tf.constant([[0.0,0.0],
+                         [0.0,0.0]])
+        pbp(x)
 
 if __name__ == "__main__":
     unittest.main()
