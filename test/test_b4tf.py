13d31929feae907a45a2a1e4132eb39f43ae2689
Yamada <ymd_h@outlook.jp>
2020-09-21 20:13:13 +0900

    Fix: import with alias name

--- test/test_b4tf.py
+++ test/test_b4tf.py
@@ -2,8 +2,8 @@ import unittest
 
 class TestB4TF(unittest.TestCase):
     def test_import_b4tf(self):
-        import b4tf
-        pbp = b4tf.models.PBP([2,1])
+        import b4tf as _b4tf
+        pbp = _b4tf.models.PBP([2,1])
 
     def test_import_PBP(self):
         from b4tf.models import PBP as _PBP
