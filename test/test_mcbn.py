f4729db2a6ac115ac27d42846973223eb1751821
Yamada <ymd_h@outlook.jp>
2020-09-27 19:28:25 +0900

    Add unit test for deep layers

--- test/test_mcbn.py
+++ test/test_mcbn.py
@@ -40,6 +40,28 @@ class TestMCBN(unittest.TestCase):
         self.assertIsInstance(cov,tf.Tensor)
         self.assertEqual(cov.shape,(1,1,1))
 
+    def test_deep_fit_predict(self):
+        mcbn = MCBN([5,4,5,1], 0.5,input_shape=(2,))
+        mcbn.compile("adam","mean_squared_error")
+
+        x = tf.constant([2.0,1.0])
+        y = tf.constant(1.0)
+        x_ = tf.constant([2.0,1.0])
+
+        mcbn.fit(x,y)
+
+        m, cov = mcbn.predict(x_)
+        self.assertIsInstance(m,tf.Tensor)
+        self.assertEqual(m.shape,(1,1))
+        self.assertIsInstance(cov,tf.Tensor)
+        self.assertEqual(cov.shape,(1,1,1))
+
+        m, cov = mcbn.predict(x_,n_batches=2)
+        self.assertIsInstance(m,tf.Tensor)
+        self.assertEqual(m.shape,(1,1))
+        self.assertIsInstance(cov,tf.Tensor)
+        self.assertEqual(cov.shape,(1,1,1))
+
 
     def test_fit_batch(self):
         mcbn = MCBN([5,3], 0.5,input_shape=(2,))
